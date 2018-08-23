39af5c7eb72a765cb5e31cae5679d1f71ce5a825
Hiroki Sakuraba <sakurabah@vivit.co.jp>
2018-08-24 03:04:54 +0900

    リファクタリング

--- cli/main.py
+++ cli/main.py
@@ -1,99 +1,120 @@
 import os
-import fire
-import yaml
 from itertools import chain
-from github import Github
+from typing import Dict, List
 
-def find(iterable, default, pred):
-    # http://docs.python.jp/3/library/itertools.html
-    return next(filter(pred, iterable), default)
+import github
+import yaml
+from fire import Fire
 
-class CLI(object):
-    def __init__(self):
-        try:
-            token = os.environ['GITHUB_TOKEN']
-        except KeyError:
-            print('Undefined `GITHUB_TOKEN` environment variable.')
-            raise SystemExit(1)
-        self.__github = Github(token)
+GITHUB_TOKEN: str = os.getenv('GITHUB_TOKEN')
+gh: github.Github = github.Github(GITHUB_TOKEN)
 
-    def sync(self, filepath='config.yaml', run=False):
-        config = Config(filepath, self.__github)
-
-        repositories = config.repositories()
-
-        print('In sync...')
-        if not run:
-            print('======= Dry Run Mode =======')
-        for repository in repositories:
-            print(repository.full_name)
-            self.__sync_label(repository, config.labels(), run)
-        print('Complete!!')
-
-    def __sync_label(self, repository, labels, run):
-        current = list(map(lambda l: l.name, repository.get_labels()))
-        new = list(map(lambda l: l.name, labels))
-
-        deletion_names = list(set(current) - set(new))
-        deletion_labels = list(filter(lambda l: l.name in deletion_names, repository.get_labels()))
-        for label in deletion_labels:
-            print('  Delete: `' + label.name + '`')
-            if run:
-                label.delete()
-
-        creation_names = list(set(new) - set(current))
-        creation_labels = list(filter(lambda l: l.name in creation_names, labels))
-        for label in creation_labels:
-            print('  Create: `' + label.name + '`')
-            if run:
-                repository.create_label(label.name, label.color, label.description)
-
-class Config(object):
-    def __init__(self, filepath, github):
-        self.__data = yaml.load(open(filepath, 'r+'))
-        self.__github = github
-        self.__labels = None
-        self.__repositories = None
-
-    def labels(self):
-        if self.__labels is None:
-            self.__labels = list(map(lambda l: Label(l), self.__data['labels']))
-        return self.__labels
-
-    def repositories(self):
-        return list(chain(self.__get_user_repos(), self.__get_organizations_repos()))
-
-    def __get_user_repos(self):
-        try:
-            state = self.__data['user']
-        except KeyError:
-            return []
-        if not state:
-            return []
-
-        repos = self.__github.get_user().get_repos()
-        user_name = self.__github.get_user().login
-        return list(filter(lambda r: r.owner.login == user_name, repos))
-
-    def __get_organizations_repos(self):
-        try:
-            orgs = self.__data['organizations']
-        except KeyError:
-            return []
-        if orgs is None:
-            return []
-        expr = map(lambda o: self.__github.get_organization(o).get_repos(), orgs)
-        return list(chain.from_iterable(expr))
 
 class Label(object):
-    def __init__(self, data):
-        try:
-            self.name = data['name']
-            self.color = data['color']
-            self.description = data['description']
-        except KeyError:
-            print('Invalid label.')
-            SystemExit(1)
+    def __init__(self, data: Dict) -> None:
+        self.name = data.get('name')
+        self.color = data.get('color')
+        self.description = data.get('descrioption')
+
+
+def load_config(filepath: str) -> Dict:
+    data = None
+
+    def __load() -> Dict:
+        nonlocal data
+        if data is None:
+            data = yaml.load(open(filepath, 'r+'))
+        return data
+
+    return __load()
+
+
+def expected_labels(data) -> List[Label]:
+    list: List[Label] = [Label(d) for d in data]
+    return list
+
+
+def user_repositories(gh: github.Github) -> List[github.Repository.Repository]:
+    username: str = gh.get_user().login
+    return [
+        r for r in gh.get_user().get_repos()
+        if (not r.archived) and (r.owner.login == username)
+    ]
+
+
+def org_repositories(orgname: str,
+                     gh: github.Github) -> List[github.Repository.Repository]:
+    return [
+        r for r in gh.get_organization(orgname).get_repos() if not r.archived
+    ]
+
+
+def target_repositories(data: Dict) -> List[github.Repository.Repository]:
+    if data.get("user"):
+        return user_repositories(gh)
+    else:
+        return chain.from_iterable([
+            org_repositories(orgname, gh)
+            for orgname in data.get('organizations')
+        ])
+
+
+def create_labels(current: List[github.Label.Label], expected: List[Label],
+                  repo: github.Repository.Repository, run: bool) -> None:
+    def __creation_plan(current: List[github.Label.Label],
+                        expected: List[Label]) -> List[Label]:
+        current_names: List[str] = [l.name for l in current]
+        expected_names: List[str] = [l.name for l in expected]
+        names: List[str] = list(set(expected_names) - set(current_names))
+        return [l for l in expected if l.name in names]
+
+    for l in __creation_plan(current, expected):
+        print('  Create: `' + l.name + '`')
+        if run:
+            repo.create_label(l.name, l.color, l.description)
+
+
+def delete_labels(current: List[github.Label.Label], expected: List[Label],
+                  run: bool) -> None:
+    def __deletion_plan(current: List[github.Label.Label],
+                        expected: List[Label]) -> List[github.Label.Label]:
+        current_names: List[str] = [l.name for l in current]
+        exptected_names: List[str] = [l.name for l in expected]
+        names = list(set(current_names) - set(exptected_names))
+        return [l for l in current if l.name in names]
+
+    for l in __deletion_plan(current, expected):
+        print('  Delete: `' + l.name + '`')
+        if run:
+            l.delete()
+
+
+def edit_labels() -> None:
+    def __edition_plan(current: List[github.Label.Label],
+                       expected: List[Label]) -> List[github.Label.Label]:
+        # TODO not implemented
+        return []
+
+    # TODO not implemented
+    pass
+
+
+def sync(filepath='config.yaml', run=False):
+    print('In sync...')
+    if not run:
+        print('======= Dry Run Mode =======')
+
+    config: Dict = load_config(filepath)
+    for repo in target_repositories(config):
+        print(repo.full_name)
+        current: List[github.Label] = repo.get_labels()
+        expected: List[Label] = expected_labels(config.get('labels'))
+
+        delete_labels(current, expected, run)
+        create_labels(current, expected, repo, run)
+
+    print('Complete!!')
+
 
 if __name__ == '__main__':
-    fire.Fire(CLI)
+    Fire({'sync': sync})
